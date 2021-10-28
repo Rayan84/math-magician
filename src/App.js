4a60f4230cc82df2b472f808af44c5765f8025ff
Rayan84 <rayan_84@live.com>
2021-10-28 11:51:39 +0200

    Add Quote Component and Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  // useRouteMatch,
+  // useParams
+} from "react-router-dom";
 
  const App = () => {
-    return( <div className="App">
-        <div>
-          <Calculator></Calculator>
-        </div>
+    return(
+      <Router>
+       <div className="App">
+         <ul>
+           <li><Link to="/">Home</Link></li>
+           <li><Link to="./calculator">Calculator</Link></li>
+           <li><Link to="./quote">Quote</Link></li>
+         </ul>
+         <Switch>
+           <Route path="/quote">
+             <Quote />
+           </Route>
+           <Route path="/Calculator">
+             <Calculator />
+           </Route>
+         </Switch>
+
       </div>
+      </Router>
     )
 };
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function Quote2() {
+//   return <h2>Quote</h2>;
+// }
+
+// function Calculator2() {
+//   return <h2>Calculator</h2>;
+// }
 
 export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Quote = () => {
+    return(
+        <div>
+            <h1>This is Quote page</h1>
+        </div>
+    );
+}
+
+export default Quote;
